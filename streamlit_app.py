c0d9c2495de20a66a96cdfca0f9f5acd70d3b9ae
ginaladybug <139566075+ginaladybug@users.noreply.github.com>
2023-07-14 08:49:15 -0600

    Update streamlit_app.py
    fixed an error

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,4 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free_range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
-streamlit.header (''🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header ('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
